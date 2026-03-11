c1bc6a3311e0dbe4d716f7447d7e1daa22af9b39
shabinvs <expelonofficial@gmail.com>
2026-03-11 18:16:46 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -483,7 +483,7 @@ const Stats = () => {
           </span>
         </div>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {stats.map((stat, idx) => (
             <motion.div
               key={idx}
